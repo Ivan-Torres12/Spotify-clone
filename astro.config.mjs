5e1b61170cdf93a387e163a39783a707a43595c0
Ivan-Torres12 <tivan5153@gmail.com>
2025-02-21 21:45:11 -0600

    update5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel(),
 });
